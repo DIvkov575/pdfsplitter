0a22c24a8455cb6ae6255a44c968a4172027ab1c
Dima I <79413560+DIvkov575@users.noreply.github.com>
2023-05-23 18:35:05 -0400

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-
+// very helpful comments aqui \/ my redundent config class ¯\_(ツ)_/¯
         Conf conf = new Conf(
                 "/Users/dmitriyivkov/Downloads/",
                 "scans/",
@@ -13,6 +13,7 @@ public class Main {
         );
         try {
 //            SplitPdf.exec(conf);
+            // runs default option w/ no conifg -> I gave up modifying config for every tiny variation in dirs etc so I did it inline
             SplitPdf.exec();
         }
         catch (IOException e) {
